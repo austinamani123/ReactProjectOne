f4415e92058c8bb804cd4a7edb1eb3cfc9fcee1b
Austin Amani <amaniaustin123@gmail.com>
2023-09-04 21:06:52 +0300

    Used prop drilling and useState  in the components

--- src/Content.js
+++ src/Content.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { useState } from 'react';
 import Square from './Square';
 import ColorInput from './ColorInput';
 
 const Content = () => {
+	const [color, setColor] = useState('');
+
+	const handleChange = (e) => {
+		setColor(e.target.value);
+	};
+
 	return (
 		<main className="Content">
-			<Square />
-			<ColorInput />
+			<Square color={color} setColor={setColor} handleChange={handleChange} />
+			<ColorInput color={color} setColor={setColor} handleChange={handleChange} />
 		</main>
 	);
 };
