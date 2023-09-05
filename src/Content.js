82720568c7408962ee4c3c295d754ff382e595ac
Austin Amani <amaniaustin123@gmail.com>
2023-09-05 21:25:47 +0300

    Started working on the background color

--- src/Content.js
+++ src/Content.js
@@ -5,14 +5,26 @@ import ColorInput from './ColorInput';
 
 const Content = () => {
 	const [color, setColor] = useState('');
+	const [divBackgroundColor, setDivBackgroundColor] = useState('');
 
 	const handleChange = (e) => {
 		setColor(e.target.value);
+
+		setDivBackgroundColor(e.target.value);
+	};
+
+	const squareStyle = {
+		backgroundcColor: divBackgroundColor,
 	};
 
 	return (
 		<main className="Content">
-			<Square color={color} setColor={setColor} handleChange={handleChange} />
+			<Square
+				color={color}
+				setColor={setColor}
+				handleChange={handleChange}
+				squareStyle={squareStyle}
+			/>
 			<ColorInput color={color} setColor={setColor} handleChange={handleChange} />
 		</main>
 	);
